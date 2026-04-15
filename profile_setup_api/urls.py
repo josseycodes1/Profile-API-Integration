725a9382435d364059c609fa4abed542b2bbfc18
Josseycodes <adewumijosephine1@gmail.com>
2026-04-15 20:08:47 +0100

    debug

--- profile_setup_api/urls.py
+++ profile_setup_api/urls.py
@@ -4,4 +4,6 @@ from .views import ProfileListCreateView, ProfileDetailView
 urlpatterns = [
     path('profiles/', ProfileListCreateView.as_view(), name='profiles'),
     path('profiles/<uuid:profile_id>/', ProfileDetailView.as_view(), name='profile-detail'),
+    path('profiles', ProfileListCreateView.as_view(), name='profiles'),
+    path('profiles/<uuid:profile_id>', ProfileDetailView.as_view(), name='profile-detail'),
 ]
